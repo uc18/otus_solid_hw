c47cc89b6f5213ae55b280ca376b935625b2a9b3
Valerij Nikitin <Ninja15@yandex.ru>
2025-02-02 19:57:43 +0400

    Somee changes
    Moved repos

---
+++ HW_Reflection_Serialization/ReflectionOtusTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ReflectionOtusTask;
+
+class Program
+{
+    static void Main()
+    {
+        const int firstCount = 100000;
+        var serializator = new Serialization();
+        var serializeClass = new SomeSerializationClass();
+
+        var timer = new Stopwatch();
+        timer.Start();
+        for (var i = 0; i < firstCount; i++)
+        {
+            var serliazationString = serializator.GetString(serializeClass);
+        }
+
+        timer.Stop();
+        Console.WriteLine(timer.ElapsedMilliseconds);
+
+        var anotherClass = serializeClass.Get();
+
+        timer.Start();
+        for (var i = 0; i < firstCount; i++)
+        {
+            var anotherString = serializator.GetString(anotherClass);
+        }
+
+        timer.Stop();
+        Console.WriteLine(timer.ElapsedMilliseconds);
+    }
+}
---
+++ HW_Reflection_Serialization/ReflectionOtusTask/SomeSerializationClass.cs
@@ -0,0 +1,22 @@
+﻿namespace ReflectionOtusTask;
+
+public class SomeSerializationClass
+{
+    public int i1;
+    public int i2;
+    public int i3;
+    public int i4;
+    public int i5;
+
+    public SomeSerializationClass Get()
+    {
+        return new SomeSerializationClass
+        {
+            i1 = 1,
+            i2 = 2,
+            i3 = 3,
+            i4 = 4,
+            i5 = 5
+        };
+    }
+}
